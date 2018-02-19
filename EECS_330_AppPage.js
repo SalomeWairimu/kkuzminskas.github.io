776a412697acea3eda1bf9ebd0f77a9d02ef6a0f
Jack Warshaw <jackwarshaw10@gmail.com>
2018-02-18 18:49:46 -0600

    Howdy

--- EECS_330_AppPage.js
+++ EECS_330_AppPage.js
@@ -1,3 +1,12 @@
-function  graph_swap(){
-	
+function  graph_swap_monthly(){
+	document.getElementById("graph").innerHTML = "/images/mock_monthly_graph.jpeg";
+}
+
+
+function graph_swap_yearly(){
+	document.getElementById("graph").innerHTML = "/images/mock_graph.jpeg";
+}
+
+function graph_swap_weekly(){
+	document.getElementById("graph").innerHTML = "/images/mock_weekly_graph.jpeg";
 }
