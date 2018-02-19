2359bd577a05d6f839f2b5d00698252d931ed272
kkuzminskas <kkuzminskas@icloud.com>
2018-02-18 22:33:33 -0600

    images

--- EECS_330_AppPage.js
+++ EECS_330_AppPage.js
@@ -1,12 +1,12 @@
 function  graph_swap_monthly(){
-	document.getElementById("graph").innerHTML = <img src="/images/mock_monthly_graph.jpeg">;
+	document.getElementById("graph").src ="/images/mock_monthly_graph.jpeg";
 }
 
 
 function graph_swap_yearly(){
-	document.getElementById("graph").innerHTML = <img src="/images/mock_graph.jpeg">;
+	document.getElementById("graph").src = "/images/mock_graph.jpeg";
 }
 
 function graph_swap_weekly(){
-	document.getElementById("graph").innerHTML = <img src="/images/mock_weekly_graph.jpeg">;
+	document.getElementById("graph").src = "/images/mock_weekly_graph.jpeg";
 }
