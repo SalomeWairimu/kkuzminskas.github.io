085ee3f61c03b20b25daa52f8045dd5d8a56c61b
Jack Warshaw <jackwarshaw10@gmail.com>
2018-02-28 19:59:00 -0600

    k

--- EECS_330_AppPage.js
+++ EECS_330_AppPage.js
@@ -10,3 +10,19 @@ function graph_swap_yearly(){
 function graph_swap_weekly(){
 	document.getElementById("graph").src = "/images/mock_weekly_graph.jpeg";
 }
+
+function selected_graph(){
+	if (document.getElementById("graph").src = "/images/mock_monthly_graph.jpeg"){
+		document.getElementById("graph_monthly_button").style.background = "black";
+		document.getElementById("graph_monthly_button").style.color = "#20A4F3";
+	}
+	if (document.getElementById("graph").src = "/images/mock_yearly_graph.jpeg"){
+		document.getElementById("graph_yearly_button").style.background = "black";
+		document.getElementById("graph_yearly_button").style.color = "#20A4F3";
+	}
+	if (document.getElementById("graph").src = "/images/mock_weekly_graph.jpeg"){
+		document.getElementById("graph_weekly_button").style.background = "black";
+		document.getElementById("graph_weekly_button").style.color = "#20A4F3";
+	}	
+
+}
