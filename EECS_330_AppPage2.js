4a27acc83c92eb3fc67e852667858ffd69b33eeb
Jack Warshaw <jackwarshaw10@gmail.com>
2018-03-07 01:49:18 -0600

    jw

--- EECS_330_AppPage2.js
+++ EECS_330_AppPage2.js
@@ -8,5 +8,5 @@ function graph_swap_yearly(){
 }
 
 function graph_swap_weekly(){
-	document.getElementById("graph").src = "/images/EECS_330_Spotify_ThisWeekly_Graph.jpg";
+	document.getElementById("graph").src = "/images/EECS_330_Spotify_ThisWeek_Graph.jpg";
 }
