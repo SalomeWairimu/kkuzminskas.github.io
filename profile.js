5f962c37aaa6045e6f68a3f78c67d6a225bff801
kkuzminskas <kkuzminskas@icloud.com>
2018-03-12 09:17:15 -0500

    fda

--- profile.js
+++ profile.js
@@ -1,7 +1,8 @@
 var eye_off="";
 
 
-function changeVisi(id, id2)
+
+function changeVisi(id, id2,id3)
 {
 	eye_off=document.getElementById(id).innerHTML;
 
@@ -9,15 +10,23 @@ function changeVisi(id, id2)
    {
    		eye_off="visibility";
    		text_off="Friends";
+   		color="white";
+   		opacity="1";
+
    }
-  else
+
+   else
    {
    		eye_off="visibility_off";
    		text_off="Only Me";
+   		color="#e7e7e7";
+   		opacity="0.7";
 
    }
 
    document.getElementById(id).innerHTML=eye_off;
    document.getElementById(id2).innerHTML=text_off;
+   document.getElementById(id3).style.background=color;
+   document.getElementById(id3).style.opacity=opacity;
 
 }
