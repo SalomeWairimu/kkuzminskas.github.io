9931212b5682d83b6519704a02485cb150420e16
Salome Wairimu <salomekariuki2020@u.northwestern.edu>
2018-03-14 23:38:38 -0500

    corrected color on tabs

--- EECS_330_AppPage2.js
+++ EECS_330_AppPage2.js
@@ -1,45 +1,39 @@
 function  graph_swap_monthly(){
-	document.getElementById("graph").src = "/images/EECS_330_Spotify_ThisMonth_Graph.jpg";
-	document.getElementById("graph").src = "/images/EECS_330_Netflix_ThisMonth_Graph.jpg";
-	var btnContainer = document.getElementById("btn-cnt");
-            var btns = btnContainer.getElementsByClassName("graph_button");
-            for (var i = 0; i < btns.length; i++) {
-              btns[i].addEventListener("click", function(){
-                var current = document.getElementsByClassName("graph_button active");
-                current[0].className = current[0].className.replace("graph_button active", "graph_button");
-                this.className += " active";
-            
-              });
-            }
+  document.getElementById("graph").src = "/images/EECS_330_Netflix_ThisMonth_Graph.jpg";
+  var yearly=document.getElementById("graph_yearly_button");
+  var weekly=document.getElementById("graph_weekly_button");
+  var monthly=document.getElementById("graph_monthly_button");
+  yearly.className="graph_button";
+  weekly.className="graph_button";
+  monthly.className="graph_button active";
+
 }
 
 
 function graph_swap_yearly(){
-	document.getElementById("graph").src = "/images/EECS_330_Spotify_ThisYear_Graph.jpg";
-	document.getElementById("graph").src = "/images/EECS_330_Netflix_ThisMonth_Graph.jpg";
-	var btnContainer = document.getElementById("btn-cnt");
-            var btns = btnContainer.getElementsByClassName("graph_button");
-            for (var i = 0; i < btns.length; i++) {
-              btns[i].addEventListener("click", function(){
-                var current = document.getElementsByClassName("graph_button active");
-                current[0].className = current[0].className.replace("graph_button active", "graph_button");
-                this.className += " active";
-            
-              });
-            }
+	document.getElementById("graph").src = "/images/EECS_330_Netflix_ThisYear_Graph.jpg";
+  var yearly=document.getElementById("graph_yearly_button");
+  var weekly=document.getElementById("graph_weekly_button");
+  var monthly=document.getElementById("graph_monthly_button");
+  yearly.className="graph_button active";
+  weekly.className="graph_button";
+  monthly.className="graph_button";
 }
 
 function graph_swap_weekly(){
-	document.getElementById("graph").src = "/images/EECS_330_Spotify_ThisWeek_Graph.jpg";
-	document.getElementById("graph").src = "/images/EECS_330_Netflix_ThisMonth_Graph.jpg";
-	var btnContainer = document.getElementById("btn-cnt");
-            var btns = btnContainer.getElementsByClassName("graph_button");
-            for (var i = 0; i < btns.length; i++) {
-              btns[i].addEventListener("click", function(){
-                var current = document.getElementsByClassName("graph_button active");
-                current[0].className = current[0].className.replace("graph_button active", "graph_button");
-                this.className += " active";
-            
-              });
-            }
+	document.getElementById("graph").src = "/images/EECS_330_Netflix_ThisWeek_Graph.jpg";
+  var yearly=document.getElementById("graph_yearly_button");
+  var weekly=document.getElementById("graph_weekly_button");
+  var monthly=document.getElementById("graph_monthly_button");
+  yearly.className="graph_button";
+  weekly.className="graph_button  active";
+  monthly.className="graph_button";
+}
+
+function change_button_hover(){
+
+}
+
+function change_button_click(){
+	
 }
