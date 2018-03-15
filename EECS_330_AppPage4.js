b8765f179558adf75441b62a13aecde8491b8af9
Jack Warshaw <jackwarshaw10@gmail.com>
2018-03-15 04:11:44 -0500

    jw

---
+++ EECS_330_AppPage4.js
@@ -0,0 +1,12 @@
+function  graph_swap_monthly(){
+	document.getElementById("graph").src = "/images/EECS_330_Amazon_ThisMonth_Graph.jpg";
+}
+
+
+function graph_swap_yearly(){
+	document.getElementById("graph").src = "/images/EECS_330_Amazon_ThisYear_Graph.jpg";
+}
+
+function graph_swap_weekly(){
+	document.getElementById("graph").src = "/images/EECS_330_Amazon_ThisWeek_Graph.jpg";
+}
