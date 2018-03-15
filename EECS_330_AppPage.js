eb0e9ea142756d2c66be82edd7581b67199d06e7
Salome Wairimu <salomekariuki2020@u.northwestern.edu>
2018-03-14 21:53:52 -0500

    filter works perfectly

--- EECS_330_AppPage.js
+++ EECS_330_AppPage.js
@@ -1,14 +1,46 @@
 function  graph_swap_monthly(){
 	document.getElementById("graph").src = "/images/EECS_330_Netflix_ThisMonth_Graph.jpg";
+	var btnContainer = document.getElementById("btn-cnt");
+            var btns = btnContainer.getElementsByClassName("graph_button");
+            for (var i = 0; i < btns.length; i++) {
+              btns[i].addEventListener("click", function(){
+                var current = document.getElementsByClassName("graph_button active");
+                current[0].className = current[0].className.replace("graph_button active", "graph_button");
+                this.className += " active";
+            
+              });
+            }
 }
 
 
 function graph_swap_yearly(){
 	document.getElementById("graph").src = "/images/EECS_330_Netflix_ThisYear_Graph.jpg";
+	document.getElementById("graph").src = "/images/EECS_330_Netflix_ThisMonth_Graph.jpg";
+	var btnContainer = document.getElementById("btn-cnt");
+            var btns = btnContainer.getElementsByClassName("graph_button");
+            for (var i = 0; i < btns.length; i++) {
+              btns[i].addEventListener("click", function(){
+                var current = document.getElementsByClassName("graph_button active");
+                current[0].className = current[0].className.replace("graph_button active", "graph_button");
+                this.className += " active";
+            
+              });
+            }
 }
 
 function graph_swap_weekly(){
 	document.getElementById("graph").src = "/images/EECS_330_Netflix_ThisWeek_Graph.jpg";
+	document.getElementById("graph").src = "/images/EECS_330_Netflix_ThisMonth_Graph.jpg";
+	var btnContainer = document.getElementById("btn-cnt");
+            var btns = btnContainer.getElementsByClassName("graph_button");
+            for (var i = 0; i < btns.length; i++) {
+              btns[i].addEventListener("click", function(){
+                var current = document.getElementsByClassName("graph_button active");
+                current[0].className = current[0].className.replace("graph_button active", "graph_button");
+                this.className += " active";
+            
+              });
+            }
 }
 
 function change_button_hover(){
--- EECS_330_AppPage2.js
+++ EECS_330_AppPage2.js
@@ -1,12 +1,45 @@
 function  graph_swap_monthly(){
 	document.getElementById("graph").src = "/images/EECS_330_Spotify_ThisMonth_Graph.jpg";
+	document.getElementById("graph").src = "/images/EECS_330_Netflix_ThisMonth_Graph.jpg";
+	var btnContainer = document.getElementById("btn-cnt");
+            var btns = btnContainer.getElementsByClassName("graph_button");
+            for (var i = 0; i < btns.length; i++) {
+              btns[i].addEventListener("click", function(){
+                var current = document.getElementsByClassName("graph_button active");
+                current[0].className = current[0].className.replace("graph_button active", "graph_button");
+                this.className += " active";
+            
+              });
+            }
 }
 
 
 function graph_swap_yearly(){
 	document.getElementById("graph").src = "/images/EECS_330_Spotify_ThisYear_Graph.jpg";
+	document.getElementById("graph").src = "/images/EECS_330_Netflix_ThisMonth_Graph.jpg";
+	var btnContainer = document.getElementById("btn-cnt");
+            var btns = btnContainer.getElementsByClassName("graph_button");
+            for (var i = 0; i < btns.length; i++) {
+              btns[i].addEventListener("click", function(){
+                var current = document.getElementsByClassName("graph_button active");
+                current[0].className = current[0].className.replace("graph_button active", "graph_button");
+                this.className += " active";
+            
+              });
+            }
 }
 
 function graph_swap_weekly(){
 	document.getElementById("graph").src = "/images/EECS_330_Spotify_ThisWeek_Graph.jpg";
+	document.getElementById("graph").src = "/images/EECS_330_Netflix_ThisMonth_Graph.jpg";
+	var btnContainer = document.getElementById("btn-cnt");
+            var btns = btnContainer.getElementsByClassName("graph_button");
+            for (var i = 0; i < btns.length; i++) {
+              btns[i].addEventListener("click", function(){
+                var current = document.getElementsByClassName("graph_button active");
+                current[0].className = current[0].className.replace("graph_button active", "graph_button");
+                this.className += " active";
+            
+              });
+            }
 }
